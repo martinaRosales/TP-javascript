ac372eab00b49d975b75b3473fa6f9a3b13de7ab
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-04-14 17:58:37 -0300

    changes

--- Ej1.js
+++ Ej1.js
@@ -5,9 +5,12 @@ submitBtn.addEventListener('click', function(){
     //cual es la diferencia entre querySelector y getElementById?
     string = document.getElementById('randomString').value;
     if (string){
-        string = string.replace(/\s+/g, '') ;
+        //string = string.replace(/\s+/g, '') ;
 
-        console.log('hay ', string.length, 'caracteres en su texto, sin incluir los espacios')
+        //console.log('hay ', string.length, 'caracteres en su texto, sin incluir los espacios')
+        if (string.length>100){
+            console.log('hay más de 100 caracteres, por favor reduzca la cantidad')
+        }
     } else {
         console.log('hay 0 caracteres en su texto')
     }
